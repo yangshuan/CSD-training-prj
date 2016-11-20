3ec686eebd376cf0a835c26b4936cbfc0be3f548
Yang Shuan <shuayang@paypal.com>
2016-11-20 14:58:27 +0800

    fix withdraw bug

---
+++ src/main/java/example/banking/DebitAccount.java
@@ -0,0 +1,7 @@
+package example.banking;
+
+/**
+ * Created by shuayang on 16/11/20.
+ */
+public class DebitAccount {
+}
---
+++ src/test/java/example/banking/AccountTest.java
@@ -0,0 +1,65 @@
+package example.banking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+    @Test
+    public void openDebitAccountIsNotNull() throws Exception {
+        assertNotNull(Account.openDebitAccount());
+    }
+
+    @Test
+    public void openAccountIsNotNull() throws Exception {
+        assertNotNull(Account.openAccount());
+    }
+
+    @Test
+    public void balanceIs100AfterDeposit100() throws Exception {
+        Account account = Account.openAccount();
+        account.deposit(100);
+
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    public void withdraw60FromAccountAfterDeposit100ThenRemaiding40() throws Exception {
+        Account account = Account.openAccount();
+        account.deposit(100);
+        account.withdraw(60);
+
+        assertEquals(40, account.getBalance());
+    }
+
+    @Test
+    public void withdraw60FromAccountAfterDeposit50ThenRemaidingMinus10() throws Exception {
+        Account account = Account.openAccount();
+        account.deposit(50);
+        account.withdraw(60);
+
+        assertEquals(-10, account.getBalance());
+    }
+
+    @Test
+    public void withdraw60FromDebitAccountAfterDeposit100ThenRemaiding40() throws Exception {
+        Account account = Account.openDebitAccount();
+        account.deposit(100);
+        account.withdraw(60);
+
+        assertEquals(40, account.getBalance());
+    }
+
+    @Test
+    public void withdraw60FromDebitAccountAfterDeposit50ThenRemaidingMinus10() throws Exception {
+        Account account = Account.openDebitAccount();
+        account.deposit(50);
+        try {
+            account.withdraw(60);
+        } catch (Exception e) {
+            assertEquals(InsufficientFundsException.class, e.getClass());
+        }
+
+        assertEquals(50, account.getBalance());
+    }
+}
