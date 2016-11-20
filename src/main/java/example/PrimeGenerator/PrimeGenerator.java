fcdc15b9cdc9b78ebc911fe1fcb1831ffbca1f45
Yang Shuan <shuayang@paypal.com>
2016-11-20 14:39:10 +0800

    after refactor using java8

--- src/main/java/example/PrimeGenerator/PrimeGenerator.java
+++ src/main/java/example/PrimeGenerator/PrimeGenerator.java
@@ -1,5 +1,7 @@
 package example.PrimeGenerator;
 
+import java.util.stream.IntStream;
+
 /**
  * Refactorings:
  * 
@@ -22,44 +24,15 @@ public class PrimeGenerator {
 	 * @return
 	 */
 	public int[] generatePrimes(int maxValue) {
-		if (maxValue >= 2) { // the only valid case
-			// declarations
-			int s = maxValue + 1; // size of array
-			boolean[] f = new boolean[s];
-
-			// initialize array to true
-			for (int i = 0; i < s; i++) {
-				f[i] = true;
-			}
-
-			// get rid of known non-primes
-			f[0] = f[1] = false;
-
-			// sieve
-			for (int i = 2; i < Math.sqrt(s) + 1; i++) {
-				for (int j = 2 * i; j < s; j += i) {
-					f[j] = false; // multiple is not prime
-				}
-			}
-
-			// how many primes are there?
-			int count = 0;
-			for (int i = 0; i < s; i++) {
-				if (f[i])
-					count++; // bump count
-			}
-
-			int[] primes = new int[count];
-
-			// move the primes into the result
-			for (int i = 0, j = 0; i < s; i++) {
-				if (f[i])
-					primes[j++] = i;
-			}
-
-			return primes;
-		} else { // maxValue < 2
-			return new int[0]; // return null array if bad input.
-		}
+		return IntStream.range(2, maxValue + 1)
+				.filter(value -> {
+					for (int i = 2; i <= value / 2; i++) {
+						if (value % i == 0) {
+							return false;
+						}
+					}
+					return true;
+				})
+				.toArray();
 	}
 }
